2acb13cbfbe50e2fff3b1fd73e5df3793f2fa59b
Naveennayak022 <naveennayak022@gmail.com>
2023-09-06 12:29:22 +0530

    changes

--- js2.js
+++ js2.js
@@ -1,6 +1,12 @@
 const prompt = require("prompt-sync")();
-function greetPerson(name) {
+/*function greetPerson(name) {
   console.log("Hello, " + name + "!");
+}
+var userName = prompt("Please enter your name:");
+greetPerson(userName);*/
+var salutation = "Hello";
+function greetPerson(name) {
+  console.log(salutation + ", " + name + "!");
 }
 var userName = prompt("Please enter your name:");
 greetPerson(userName);
