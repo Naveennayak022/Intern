108a2c0d69c1cf9e7403e48cd03298f014979189
Naveennayak022 <naveennayak022@gmail.com>
2023-10-03 11:33:28 +0530

    new file

---
+++ my-app/calci/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Math from './Math';
+export default function App() {
+  
+  return (
+    <div>
+      <div><Math/></div>
+     
+    </div>
+  )
+}
